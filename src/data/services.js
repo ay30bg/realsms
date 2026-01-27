e6b4014cbe1e1db15a1a18233158802308023904
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-01-27 16:42:20 +0100

    Update services.js

--- src/data/services.js
+++ src/data/services.js
@@ -1,3 +1,8 @@
+import WhatsAppIcon from "../assets/whatsapp.png";
+import FacebookIcon from "../assets/facebook.png";
+import TelegramIcon from "../assets/telegram.png";
+import TwitterIcon from "../assets/twitter.png";
+
 // List of available servers with country codes
 export const servers = [
   { id: 1, name: "Nigeria", countryCode: "+234" },
@@ -9,20 +14,19 @@ export const servers = [
 // List of services with serverId linking them to a server
 export const services = [
   // Nigeria
-  { id: 1, name: "WhatsApp", price: 500, serverId: 1, stock: 15, icon: "whatsapp.png" },
-  { id: 2, name: "Facebook", price: 400, serverId: 1, stock: 12, icon: "facebook.png" },
-  { id: 3, name: "Telegram", price: 350, serverId: 1, stock: 10, icon: "telegram.png" },
+  { id: 1, name: "WhatsApp", price: 500, serverId: 1, stock: 15, icon: WhatsAppIcon },
+  { id: 2, name: "Facebook", price: 400, serverId: 1, stock: 12, icon: FacebookIcon },
+  { id: 3, name: "Telegram", price: 350, serverId: 1, stock: 10, icon: TelegramIcon },
 
   // USA
-  { id: 4, name: "WhatsApp", price: 800, serverId: 2, stock: 8, icon: "whatsapp.png" },
-  { id: 5, name: "Facebook", price: 700, serverId: 2, stock: 6, icon: "facebook.png" },
-  { id: 6, name: "X / Twitter", price: 900, serverId: 2, stock: 5, icon: "twitter.png" },
+  { id: 4, name: "WhatsApp", price: 800, serverId: 2, stock: 8, icon: WhatsAppIcon },
+  { id: 5, name: "Facebook", price: 700, serverId: 2, stock: 6, icon: FacebookIcon },
+  { id: 6, name: "X / Twitter", price: 900, serverId: 2, stock: 5, icon: TwitterIcon },
 
   // UK
-  { id: 7, name: "WhatsApp", price: 750, serverId: 3, stock: 7, icon: "whatsapp.png" },
-  { id: 8, name: "Telegram", price: 600, serverId: 3, stock: 4, icon: "telegram.png" },
-
+  { id: 7, name: "WhatsApp", price: 750, serverId: 3, stock: 7, icon: WhatsAppIcon },
+  { id: 8, name: "Telegram", price: 600, serverId: 3, stock: 4, icon: TelegramIcon },
   // India
-  { id: 9, name: "WhatsApp", price: 400, serverId: 4, stock: 20, icon: "whatsapp.png" },
-  { id: 10, name: "Facebook", price: 350, serverId: 4, stock: 18, icon: "facebook.png" },
+  { id: 9, name: "WhatsApp", price: 400, serverId: 4, stock: 20, icon: WhatsAppIcon },
+  { id: 10, name: "Facebook", price: 350, serverId: 4, stock: 18, icon: FacebookIcon },
 ];
