eeee015465f588a3c2fcdbb804d256cb4d787b52
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-02 17:50:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import FundWallet from "./pages/FundWallet";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 import Support from "./pages/Support";
 import OpayFund from "./pages/OpayFund";
 
@@ -25,6 +26,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
 
         {/* Protected Pages inside Layout */}
         <Route
