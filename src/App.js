5cc58dc2019836af9d72057df6d49be47be11e60
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-02 17:55:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
 
         {/* Protected Pages inside Layout */}
         <Route
