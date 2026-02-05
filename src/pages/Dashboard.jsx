be68530fc2d7ccc666b43bcc68bb539c64c11ea6
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-05 15:46:37 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-CB import React, { useEffect } from "react";
+'import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import StatCard from "../components/StatCard";
 import { FiCreditCard, FiShoppingCart, FiCheckCircle } from "react-icons/fi";
@@ -64,4 +64,5 @@ const Dashboard = ({ darkMode }) => {
 };
 
 export default Dashboard;
+
 
